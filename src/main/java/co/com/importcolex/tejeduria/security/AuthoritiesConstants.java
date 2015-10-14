8b1067b69f92a25a9888c00ff09d74f7e4d12a42
TalosVictorPerez <victor.perez@mozido.com>
2015-10-14 11:50:55 -0500

    IMPCOL-001 add authorities

--- src/main/java/co/com/importcolex/tejeduria/security/AuthoritiesConstants.java
+++ src/main/java/co/com/importcolex/tejeduria/security/AuthoritiesConstants.java
@@ -10,6 +10,10 @@ public final class AuthoritiesConstants {
     public static final String USER = "ROLE_USER";
 
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
+    
+    public static final String OPERARIO = "ROLE_OPERARIO";
+    
+    public static final String COMPRAS = "ROLE_COMPRAS";
 
     private AuthoritiesConstants() {
     }
